040c7586a74c45ec6acc9bee602e7a2025c81cb2
mdnomaan-sys <mdnomaan0075@gmail.com>
2026-05-10 13:28:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,18 @@ function changeText() {
   document.getElementById("text").innerText =
     "You just used JavaScript 😎";
 }
+
+let user = {
+  name: "Tommy",
+  age: 18
+};
+
+function checkUser() {
+  if (user.age >= 18) {
+    document.getElementById("output").innerText =
+      user.name + " is eligible ✅";
+  } else {
+    document.getElementById("output").innerText =
+      user.name + " is not eligible ❌";
+  }
+}
